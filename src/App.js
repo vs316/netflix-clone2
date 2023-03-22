4ec1f356908cd24a300efc3112b35b3f936187e8
VACHAN SHETTY <vachan316@gmail.com>
2023-03-23 01:30:41 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import gsap from "gsap";
+import React, { useEffect } from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import "./Cursor.css";
 
 function App() {
+  useEffect(() => {
+    const bigBall = document.querySelector(".cursor__big-ball");
+    const smallBall = document.querySelector(".cursor__small-ball");
+    const hoverables = document.querySelectorAll(".hoverable");
+    if (!bigBall || !smallBall || !hoverables) return;
+    // create an array of colors
+    const colors = ["#ff0000", "#00ff00", "#0000ff"];
+
+    // set an interval to change the border color after every 3 seconds
+    setInterval(() => {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      gsap.set(smallBall, { borderColor: randomColor });
+    }, 3000);
+    // Listeners
+    document.body.addEventListener("mousemove", onMouseMove);
+    for (let i = 0; i < hoverables.length; i++) {
+      hoverables[i].addEventListener("mouseenter", onMouseHover);
+      hoverables[i].addEventListener("mouseleave", onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+      gsap.to(bigBall, {
+        duration: 0.4,
+        x: e.clientX - bigBall.clientWidth / 2,
+        y: e.clientY - bigBall.clientHeight / 2,
+      });
+
+      gsap.to(smallBall, {
+        duration: 0.1,
+        x: e.clientX - smallBall.clientWidth / 2,
+        y: e.clientY - smallBall.clientHeight / 2,
+      });
+    }
+
+    // Hover an element
+    function onMouseHover() {
+      gsap.to(bigBall, {
+        duration: 0.3,
+        scale: 4,
+      });
+    }
+
+    function onMouseHoverOut() {
+      gsap.to(bigBall, {
+        duration: 0.3,
+        scale: 1,
+      });
+    }
+  }, []); // Add an empty dependency array to run the effect only once
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="cursor">
+        <div className="cursor__ball cursor__big-ball">
+          <svg height="30" width="30">
+            <circle cx="15" cy="15" r="12" strokeWidth="0"></circle>
+          </svg>
+        </div>
+
+        <div className="cursor__ball cursor__small-ball">
+          <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4" strokeWidth="0"></circle>
+          </svg>
+        </div>
+      </div>
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/Banner.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "./axios";
+import requests from "./requests";
+import "./Banner.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+function Banner() {
+  const [movie, setMovie] = useState({});
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [isAddedToList, setIsAddedToList] = useState(false);
+  const [savedMovies, setSavedMovies] = useState([]);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const savedMoviesStr = localStorage.getItem("savedMovies");
+    if (savedMoviesStr) {
+      setSavedMovies(JSON.parse(savedMoviesStr));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("savedMovies", JSON.stringify(savedMovies));
+  }, [savedMovies]);
+
+  useEffect(() => {
+    function handleOutsideClick(event) {
+      if (videoRef.current && !videoRef.current.contains(event.target)) {
+        setTrailerUrl("");
+      }
+    }
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [videoRef]);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  const handlePlayClick = async () => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      try {
+        const url = await movieTrailer(
+          movie?.title || movie?.name || movie?.original_name || ""
+        );
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleAddToListClick = () => {
+    setIsAddedToList(!isAddedToList);
+    const movieToAdd = {
+      id: movie.id,
+      title: movie.title || movie.name || movie.original_name,
+      overview: movie.overview,
+      poster_path: movie.poster_path,
+      backdrop_path: movie.backdrop_path,
+      vote_average: movie.vote_average,
+      release_date: movie.release_date,
+    };
+    if (!isAddedToList) {
+      setSavedMovies([...savedMovies, movieToAdd]);
+    } else {
+      setSavedMovies(savedMovies.filter((item) => item.id !== movie.id));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button" onClick={handlePlayClick}>
+            Play
+          </button>
+          <button
+            className={`banner__button ${
+              isAddedToList ? "banner__button--active" : ""
+            }`}
+            onClick={handleAddToListClick}
+          >
+            {isAddedToList ? "Added to List" : "Add to List"}
+          </button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+        <div className="video__container" ref={videoRef}>
+          {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+        </div>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
+}
+export default Banner;
